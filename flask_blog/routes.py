4f44cbada87c3b02b1eb662ed405f1541d99976e
jdboachie <judeboachiekwame@gmail.com>
2023-10-05 23:49:02 +0000

    feat: add photo resizing

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 
 from flask import render_template, url_for, flash, redirect, request
 from flask_blog import app, db, bcrypt
@@ -79,7 +80,10 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
     return picture_fn
 
 @app.route("/account", methods=['GET', 'POST'])
