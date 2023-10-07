0a4cfc477fad7d4c38a5c333daf927c370cf32de
jdboachie <judeboachiekwame@gmail.com>
2023-10-07 13:47:07 +0000

    feat: add blueprints and config

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,21 +1,35 @@
-from datetime import datetime
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_blog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a0109dd7dd9a87ace7bffc370b275759'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login' # type: ignore
-login_manager.login_message_category = 'info' # type: ignore
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
-from flask_blog import routes
+mail = Mail()
 
-def create_app():
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flask_blog.users.routes import users
+    from flask_blog.posts.routes import posts
+    from flask_blog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
     return app
